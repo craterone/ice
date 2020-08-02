54d391a34137199f592b11910a5ebf92944ffc8c
Chen Cong <craterone11@gmail.com>
2020-08-02 17:41:52 +0900

    add host ice

--- agent.go
+++ agent.go
@@ -1,5 +1,10 @@
 package ice
 
+import (
+	"fmt"
+	"net"
+)
+
 type Agent struct {
 }
 
@@ -8,6 +13,26 @@ func NewAgent() (*Agent, error) {
 	return agent, nil
 }
 
+//https://github.com/pion/transport/blob/a27a8ecf59bcc276ad5d783d8dc99d3406b81591/vnet/net.go
+func  (a *Agent) hostCandidates() {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Print(fmt.Errorf("localAddresses: %+v\n", err.Error()))
+		return
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			fmt.Print(fmt.Errorf("localAddresses: %+v\n", err.Error()))
+			continue
+		}
+		for _, a := range addrs {
+			fmt.Println(a.String())
+		}
+	}
+}
+
 func (a *Agent) GatherCandidates() error {
+	a.hostCandidates()
 	return nil
 }
